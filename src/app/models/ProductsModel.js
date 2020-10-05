06ebaa8b59330a1c6f31699da17e7e89111290a2
Jarom Antony Aidukaitis <jaaidukaitis@gmail.com>
2020-10-05 19:12:43 -0300

    Product Model created

---
+++ src/app/models/ProductsModel.js
@@ -0,0 +1,51 @@
+const dataProductDao = require('../../config/DAO/ProductsDAO');
+const db = require('../../config/database/database');
+
+const instanceDao = new dataProductDao(db);
+
+class ProductModel {
+	static getAllProducts() {
+		return instanceDao.getAllProducts();
+	}
+
+	static getOneProduct(id) {
+		return instanceDao.getOneProduct(id);
+	}
+
+	static addProduct(rating, name, description, price, in_stock, supplier_id) {
+		return instanceDao.addProduct(
+			rating,
+			name,
+			description,
+			price,
+			in_stock,
+			supplier_id
+		);
+	}
+
+	static updateProduct(
+		rating,
+		name,
+		description,
+		price,
+		in_stock,
+		supplier_id,
+		id
+	) {
+		return instanceDao.updateProduct(
+			rating,
+			name,
+			description,
+			price,
+			in_stock,
+			supplier_id,
+			id
+		);
+	}
+
+	static deleteProduct(id) {
+		return instanceDao.deleteProduct(id);
+	}
+}
+
+module.exports = ProductModel;
